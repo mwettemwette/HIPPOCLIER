939b80ab7a87d60e9132fc215bf00291f221dc13
Mwe <michel.courbin@cpe.fr>
2021-11-06 17:30:11 +0100

    Fin de la page principale (manque les images et le bandeau mais oklm)

--- script.js
+++ script.js
@@ -14,24 +14,10 @@ fetch("footer.html")
 
 /* Script pour la page Principale (peut être réutilisé)*/
 
-function showImage(imageId) {
-    document.getElementById(imageId).style.display='block';
-}
-
-function hideImage(imageId) {
-    document.getElementById(imageId).style.display='none';
-}
-
-function Retour(){
-    showImage('Apple');
-    showImage('Huawei');
-    showImage('Samsung');
-    hideImage('grillemodels');
-}
-
 let contenu_models={};
 let contenu_marques={};
 let contenu_promo={};
+let contenu_panier={};
 
 fetch('Models.json')
 .then(function(response){
@@ -41,19 +27,23 @@ fetch('Models.json')
     let contenu_marques=json["marques"];
     let contenu_models=json["models"];
     let contenu_promo=json["Code Promo"];
+    let contenu_panier=json["Paniertest"];
 
     let template1 = document.querySelector("#marques");
+    if (template1 != null){
     for(const i of contenu_marques){
       let clone = document.importNode(template1.content, true);
       newContent = clone.firstElementChild.innerHTML
+        .replace(/{{function}}/g, i.function)
         .replace(/{{image}}/g, i.image)
         .replace(/{{id}}/g, i.id)   
         .replace(/{{name}}/g, i.name)
         .replace(/{{prix}}/g, i.prix)
       clone.firstElementChild.innerHTML = newContent
-      document.getElementById('grillemarques').appendChild(clone);}
+      document.getElementById('grillemarques').appendChild(clone);}}
 
     let template2 = document.querySelector("#modeles");
+    if (template2 != null){
     for(const i of contenu_models){
       let clone = document.importNode(template2.content, true);
       newContent = clone.firstElementChild.innerHTML
@@ -63,14 +53,60 @@ fetch('Models.json')
         .replace(/{{id}}/g, i.id)
         .replace(/{{name}}/g, i.name)
       clone.firstElementChild.innerHTML = newContent
-      document.getElementById('grillemodels').appendChild(clone);
-  }
+      document.getElementById('grillemodels').appendChild(clone);}}
+
+
+    let temppanier = document.querySelector("#article");
+    if (temppanier != null){
+    for(const i of contenu_panier){
+      let clone = document.importNode(temppanier.content, true);
+
+      newContent = clone.firstElementChild.innerHTML
+        .replace(/{{image}}/g, i.image)
+        .replace(/{{name}}/g, i.name)
+      clone.firstElementChild.innerHTML = newContent
+      document.getElementById('Panier').appendChild(clone);}}
+    
+
+    let tempdetail = document.querySelector("#detailarticle");
+    if (tempdetail != null){
+    for(const i of contenu_panier){
+      let clone = document.importNode(tempdetail.content, true);
+      newContent = clone.firstElementChild.innerHTML
+        .replace(/{{name}}/g, i.name)
+        .replace(/{{nombre}}/g, i.nombre)
+      clone.firstElementChild.innerHTML = newContent
+      document.getElementById('Detail').appendChild(clone);}}
+
 })
 
 
+function waw(oui){
+  var elems = document.getElementsByClassName(oui);
+  var ret = document.getElementsByClassName('Retour');
+  var models = document.getElementById('grillemodels');
+  for (k = 0; k < models.childElementCount; k++) {
+    var model = models.children[k].children;
+    model[0].style.display = 'none';
+  }
+  ret[0].style.display='block';
+  for (i = 0; i < elems.length; i++) {
+    var elem = elems[i];
+    elem.style.display = 'block';
+  }
+}
+
+function Retour(){
+  var models = document.getElementById('grillemodels');
+  for (k = 0; k < models.childElementCount; k++) {
+    var model = models.children[k].children;
+    model[0].style.display = 'none';
+  }
+}
+
 /* Script pour la page Contact */
 
-
+/*
   function showElement() {
     var x = document.getElementById("myDIV");
     if (x.style.display === "block") {
@@ -79,7 +115,7 @@ fetch('Models.json')
       x.style.display = "block";
     }
   }
-
+*/
 /* Script pour la page Contenu de la commande*/
 
 function calculDate(date1) {
@@ -91,17 +127,13 @@ function calculDate(date1) {
     date1 = date1.split("-");
     date2 = date2.split("-");
 
-    console.log(date1, date2)
-
     if(parseInt(date1[0]) <= parseInt(date2[0])){
         if(parseInt(date1[1]) <= parseInt(date2[1])){
             if(Math.abs(parseInt(date2[2]) - parseInt(date1[2])) <= 3){
                 valRen = true;
             }
         }
-        
     }
-    console.log(valRen);
     return valRen;
 }
 
